a08ec448d6c46ba3f757afcc7660a51cdb1f3845
Steve G. Bjorg <steve.bjorg@gmail.com>
2022-03-10 21:51:49 -0700

    build all possible variations of project

--- Source/build-and-deploy.sh
+++ Source/build-and-deploy.sh
@@ -19,46 +19,174 @@ if [ -d "$BIN" ]; then
 fi
 mkdir "$BIN"
 
-OUTPUT_FOLDER="$SOURCE/MinimalFunction/bin/Release/net6.0/linux-x64/publish/"
+function build_function() {
 
-# clean-up previous build
-rm -rf "$SOURCE/MinimalFunction/bin" "$SOURCE/MinimalFunction/obj" > /dev/null 2>&1
+    # clean-up previous build
+    rm -rf "$SOURCE/$1/bin" "$SOURCE/$1/obj" > /dev/null 2>&1
 
-# build function
-dotnet publish \
-    --configuration Release \
-    --framework net6.0 \
-    --runtime linux-x64 \
-    --no-self-contained \
-    --output "$OUTPUT_FOLDER" \
-    -property:GenerateRuntimeConfigurationFiles=true \
-    -property:TieredCompilation=false \
-    -property:TieredCompilationQuickJit=false \
-    "$SOURCE/MinimalFunction/"
+    # determine framework
+    local FRAMEWORK_BUILD=""
+    local FRAMEWORK_LABEL=""
+    case $2 in
+        dotnet6)
+            FRAMEWORK_BUILD="net6.0"
+            FRAMEWORK_LABEL="Net6"
+            ;;
+        dotnetcore3.1)
+            FRAMEWORK_BUILD="netcoreapp3.1"
+            FRAMEWORK_LABEL="Core31"
+            ;;
+        *)
+            echo "Invalid framework: $1"
+            exit 1
+            ;;
+    esac
 
-# compress function output into zip file
-pushd "$OUTPUT_FOLDER"
-zip -9 -r "$BIN/LambdaPerformance-MinimalFuntion-$SUFFIX.zip" .
-popd
+    # determine architecture
+    local ARCHITECTURE_BUILD="";
+    local ARCHITECTURE_LABEL="";
+    case $3 in
+        x86_64)
+            ARCHITECTURE_BUILD="linux-x64"
+            ARCHITECTURE_LABEL="x64"
+            ;;
+        arm64)
+            ARCHITECTURE_BUILD="linux-arm64"
+            ARCHITECTURE_LABEL="Arm64"
+            ;;
+        *)
+            echo "Invalid architecture: $2"
+            exit 1
+            ;;
+    esac
+
+    # determine tiered compilation option
+    local TIERED_BUILD="";
+    local TIERED_LABEL="";
+    case $4 in
+        yes)
+            TIERED_BUILD="true";
+            TIERED_LABEL="YesTC";
+            ;;
+        no)
+            TIERED_BUILD="false";
+            TIERED_LABEL="NoTC";
+            ;;
+        *)
+            echo "Invalid tiered compilation option: $3"
+            exit 1
+            ;;
+    esac
+
+    # determine ready2run option
+    local READY2RUN_BUILD="";
+    local READY2RUN_LABEL="";
+    case $5 in
+        yes)
+            READY2RUN_BUILD="true";
+            READY2RUN_LABEL="YesR2R";
+            ;;
+        no)
+            READY2RUN_BUILD="false";
+            READY2RUN_LABEL="NoR2R";
+            ;;
+        *)
+            echo "Invalid ready2run option: $4"
+            exit 1
+            ;;
+    esac
+
+    # build project
+    echo ""
+    echo "*** BUILDING $1 [$FRAMEWORK_LABEL, $ARCHITECTURE_LABEL, $TIERED_LABEL, $READY2RUN_LABEL]"
+    OUTPUT_FOLDER="$BIN/$1-$FRAMEWORK_LABEL-$ARCHITECTURE_LABEL-$TIERED_LABEL-$READY2RUN_LABEL/"
+    dotnet publish \
+        --configuration Release \
+        --framework $FRAMEWORK_BUILD \
+        --runtime $ARCHITECTURE_BUILD \
+        --no-self-contained \
+        --output "$OUTPUT_FOLDER" \
+        -property:GenerateRuntimeConfigurationFiles=true \
+        -property:TieredCompilation=$TIERED_BUILD \
+        -property:TieredCompilationQuickJit=$TIERED_BUILD \
+        -property:PublishReadyToRun=$READY2RUN_BUILD \
+        "$SOURCE/$1/"
+    pushd "$OUTPUT_FOLDER"
+    zip -9 -r "$BIN/$1-$FRAMEWORK_LABEL-$ARCHITECTURE_LABEL-$TIERED_LABEL-$READY2RUN_LABEL-$SUFFIX.zip" .
+    popd
+}
+
+build_function MinimalFunction dotnet6 x86_64 no no
+build_function MinimalFunction dotnet6 x86_64 no yes
+build_function MinimalFunction dotnet6 x86_64 yes no
+build_function MinimalFunction dotnet6 x86_64 yes yes
+
+build_function MinimalFunction dotnet6 arm64 no no
+build_function MinimalFunction dotnet6 arm64 no yes
+build_function MinimalFunction dotnet6 arm64 yes no
+build_function MinimalFunction dotnet6 arm64 yes yes
+
+build_function MinimalFunction dotnetcore3.1 x86_64 no no
+# build_function MinimalFunction dotnetcore3.1 x86_64 no yes
+# build_function MinimalFunction dotnetcore3.1 x86_64 yes no
+# build_function MinimalFunction dotnetcore3.1 x86_64 yes yes
+
+build_function MinimalFunction dotnetcore3.1 arm64 no no
+# build_function MinimalFunction dotnetcore3.1 arm64 no yes
+# build_function MinimalFunction dotnetcore3.1 arm64 yes no
+# build_function MinimalFunction dotnetcore3.1 arm64 yes yes
+
+# build NoTieredCompilation-NoReadyToRun
+# OUTPUT_FOLDER="$BIN/MinimalFunction-Net6-x86-NoTC-NoR2R/"
+# dotnet publish \
+#     --configuration Release \
+#     --framework net6.0 \
+#     --runtime linux-x64 \
+#     --no-self-contained \
+#     --output "$OUTPUT_FOLDER" \
+#     -property:GenerateRuntimeConfigurationFiles=true \
+#     -property:TieredCompilation=false \
+#     -property:TieredCompilationQuickJit=false \
+#     -property:PublishReadyToRun=false \
+#     "$SOURCE/MinimalFunction/"
+# pushd "$OUTPUT_FOLDER"
+# zip -9 -r "$BIN/LambdaPerformance-MinimalFuntion-Net6-x86-NoTC-NoR2R-$SUFFIX.zip" .
+# popd
+
+# # build TieredCompilation-NoReadyToRun
+# OUTPUT_FOLDER="$BIN/MinimalFunction-Net6-x86-YesTC-NoR2R/"
+# dotnet publish \
+#     --configuration Release \
+#     --framework net6.0 \
+#     --runtime linux-x64 \
+#     --no-self-contained \
+#     --output "$OUTPUT_FOLDER" \
+#     -property:GenerateRuntimeConfigurationFiles=true \
+#     -property:TieredCompilation=true \
+#     -property:TieredCompilationQuickJit=true \
+#     -property:PublishReadyToRun=false \
+#     "$SOURCE/MinimalFunction/"
+# pushd "$OUTPUT_FOLDER"
+# zip -9 -r "$BIN/LambdaPerformance-MinimalFuntion-Net6-x86-YesTC-NoR2R-$SUFFIX.zip" .
+# popd
+
 
 # create or update function
-cd $BIN
-set +e
-aws lambda get-function --function-name LambdaPerformance-MinimalFunction > /dev/null 2>&1
-if [ 0 -eq $? ]; then
-    set -e
-    aws lambda update-function-code \
-        --function-name LambdaPerformance-MinimalFunction \
-        --zip-file "fileb://LambdaPerformance-MinimalFuntion-$SUFFIX.zip"
-else
-    set -e
-    aws lambda create-function \
-        --function-name LambdaPerformance-MinimalFunction \
-        --memory-size 256 \
-        --zip-file "fileb://LambdaPerformance-MinimalFuntion-$SUFFIX.zip" \
-        --handler "MinimalFunction::LambdaPerformance.MinimalFunction.Function::ProcessMessageStreamAsync" \
-        --runtime dotnet6 \
-        --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LambdaDefaultRole
-fi
-
-
+# cd $BIN
+# set +e
+# aws lambda get-function --function-name LambdaPerformance-MinimalFunction > /dev/null 2>&1
+# if [ 0 -eq $? ]; then
+#     set -e
+#     aws lambda update-function-code \
+#         --function-name LambdaPerformance-MinimalFunction \
+#         --zip-file "fileb://LambdaPerformance-MinimalFuntion-$SUFFIX.zip"
+# else
+#     set -e
+#     aws lambda create-function \
+#         --function-name LambdaPerformance-MinimalFunction \
+#         --memory-size 256 \
+#         --zip-file "fileb://LambdaPerformance-MinimalFuntion-$SUFFIX.zip" \
+#         --handler "MinimalFunction::LambdaPerformance.MinimalFunction.Function::ProcessMessageStreamAsync" \
+#         --runtime dotnet6 \
+#         --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LambdaDefaultRole
+# fi
