8e76ea79449b10d90795995e4774426cf1e4f263
Steve G. Bjorg <steve.bjorg@gmail.com>
2022-03-30 16:49:09 -0700

    SystemTextJson project

---
+++ Projects/SystemTextJson/Function.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace LambdaPerformance.SystemTextJson {
+
+    public sealed class Function {
+
+        //--- Methods ---
+        public async Task<Stream> ProcessAsync(Root request) {
+            return Stream.Null;
+        }
+    }
+}
