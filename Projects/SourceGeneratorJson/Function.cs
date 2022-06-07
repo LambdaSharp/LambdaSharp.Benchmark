57063bc8dfad336b89f178f7d6716ca629cb18b4
Steve G. Bjorg <steve.bjorg@gmail.com>
2022-06-06 18:03:58 -0700

    code style

--- Projects/SourceGeneratorJson/Function.cs
+++ Projects/SourceGeneratorJson/Function.cs
@@ -1,21 +1,19 @@
-using System.IO;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using Benchmark.SourceGeneratorJson;
 
-[assembly:LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<Benchmark.SourceGeneratorJson.FunctionSerializerContext>))]
+[assembly: LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<FunctionSerializerContext>))]
 
-namespace Benchmark.SourceGeneratorJson {
+namespace Benchmark.SourceGeneratorJson;
 
-    [JsonSerializable(typeof(Root))]
-    public partial class FunctionSerializerContext : JsonSerializerContext { }
+[JsonSerializable(typeof(Root))]
+public partial class FunctionSerializerContext : JsonSerializerContext { }
 
-    public sealed class Function {
+public sealed class Function {
 
-        //--- Methods ---
-        public async Task<Stream> ProcessAsync(Root request) {
-            return Stream.Null;
-        }
+    //--- Methods ---
+    public async Task<Stream> ProcessAsync(Root request) {
+        return Stream.Null;
     }
 }
